2c97dfeeae652b8fe972cdb4d45c86ae975b2920
jose-kunnel <jose.kunnel@hashicorp.com>
2025-08-29 14:04:03 +0530

    Create variables.tf

---
+++ modules/local_file_example/variables.tf
@@ -0,0 +1,9 @@
+variable "filename" {
+  description = "The name of the file to create"
+  type        = string
+}
+
+variable "content" {
+  description = "The content to write into the file"
+  type        = string
+}
